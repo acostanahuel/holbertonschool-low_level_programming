d1da70f6eb2de03b579bf3e1ab53aa0910e940f1
acostanahuel <acostanahuel17@gmail.com>
2023-06-15 06:26:36 -0700

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 /**
-*main-main function  "puts"
-*Return: Always 0 
-*/
+ * main - main function
+ * Return: 0
+ */
 int main(void)
-
 {
-	puts text[] = "\"Programming is like building a multilingual puzzle";
+	char text[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(text);
-	return(0);
+	return (0);
 }
