8273b0686583d3a8069ba31f992b453d47a20b1c
acostanahuel <acostanahuel17@gmail.com>
2023-06-15 08:29:56 -0700

    p or n

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+
+/* main - main function
+ * return 0
+ * */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if ( 1 > 0) {
+	print ("is positive")}
+	if (0 = 0) {
+	print ("is zero")}
+	if (-7 < 0 ) {
+	print ("is negative")}
+	return (0);
+}
