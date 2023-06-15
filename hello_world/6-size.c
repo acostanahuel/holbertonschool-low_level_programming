2eae2cfe5eb9ef2ed239ee73ff6a82fe45f44aae
acostanahuel <acostanahuel17@gmail.com>
2023-06-15 05:47:29 -0700

    fix size

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,10 +7,10 @@ int main()
     char charType;
 
     // Sizeof operator is used to evaluate the size of a variable
-    printf("Size of int: %zu bytes\n",sizeof((integerType));
-    printf("Size of float: %zu bytes\n",sizeof((floatType));
-    printf("Size of double: %zu bytes\n",sizeof((doubleType));
-    printf("Size of char: %zu byte\n",sizeof((charType));
+    printf("Size of int: %zu bytes\n",sizeof(integerType));
+    printf("Size of float: %zu bytes\n",sizeof(floatType));
+    printf("Size of double: %zu bytes\n",sizeof(doubleType));
+    printf("Size of char: %zu byte\n",sizeof(charType));
 
     return 0;
 }
