79d526a7922b9f9c1cadad50d34051345899e6fd
acostanahuel <acostanahuel17@gmail.com>
2023-06-15 05:35:27 -0700

    size.c

---
+++ hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - main function
+ * Return: 0
+ */
+int main(void)
+{	
+    int intType;
+    float floatType;
+    char charType;
+    long int longType;
+    long long int longlongType;
+
+    printf("Size of int: %zu bytes\n" , sizeof ((intType));	
+    printf ("Size of float: %zu bytes\n", sizeof ((floatType))
+    printf ("Size of char: %zu bytes\n", sizeof (charType));
+    printf ("Size of long int: %zu bytes \n", sizeof ((longType));
+    printf ("Size of long long int: %zu bytes \n", sizeof (longlongType);
+    return (0);
+
+}
