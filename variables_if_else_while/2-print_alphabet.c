6a81a9aba303570ef6df128847e142af994d497e
acostanahuel <acostanahuel17@gmail.com>
2023-06-16 08:59:57 -0700

    print alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/*
+ * main -function
+ * return: 0
+ */
+int main(void)
+{
+	char alphabet = 'a';
+	while (alphabet <= 'z')
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+	putchar('\n');
+	return (0);
+}
