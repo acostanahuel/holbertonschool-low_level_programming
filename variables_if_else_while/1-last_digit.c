3b428045e06896e92b1cec567a7c2650a52623e7
acostanahuel <acostanahuel17@gmail.com>
2023-06-16 06:31:00 -0700

    fix last d

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,25 +1,29 @@
 #include <stdlib.h>
 #include <time.h>
-/*
- * main - function 
+#include <stdio.h>
+/**
+ * main - lastd1git
  * Return: 0
  */
- int main(void)
+int main(void)
 {
 	int n;
+	int last;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	last = n % 10;
-	if (last == 0) 
+	if (last == 0)
 	{
     		printf("Last digit of %d is %d and is 0\n", n, last);
-	} else if (last > 5) 
+	}
+	else if (last > 5)
 	{
     		printf("Last digit of %d is %d and is greater than 5\n", n, last);
-	}else 
+	}
+	else
 	{
     		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
 	}
-	return 0;
-}
+	return (0);
+
