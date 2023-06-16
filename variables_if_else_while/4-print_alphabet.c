4b604377a41911551d306a86e3eacf370573f246
acostanahuel <acostanahuel17@gmail.com>
2023-06-16 15:12:22 -0700

    print alp

---
+++ variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*
+ * main -alphajjj
+ * return: 0
+ */
+int main(void)
+{
+	char alphabet = 'a';
+
+	while (alphabet => 'z');
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+	putchar('\n')
+	return 0
+}
