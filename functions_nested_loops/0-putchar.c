bf5ad1615a75080e2a468db195951fc8a6f48548
acostanahuel <acostanahuel17@gmail.com>
2023-06-17 12:58:20 -0700

    fix putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,11 +1,11 @@
 #include <unistd.h>
-/*
+/**
  *main - main function prints putchar
  *Return: 0
  */
 int main(void)
 {
-	const char* text = "_putchar\n";
+	const char *text = "_putchar\n";
 	write (1, text, 9);
 
 	return (0);
