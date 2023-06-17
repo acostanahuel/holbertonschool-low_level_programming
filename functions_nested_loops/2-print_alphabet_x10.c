57c49e491e97709c8ecbd1719ac9d8858c8d1f6b
acostanahuel <acostanahuel17@gmail.com>
2023-06-17 15:57:31 -0700

    alphx10

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 #include "main.h"
-/**
- * print - print alphabetx10
- *
- * description: akdjsal
- */
-void print_alphabet_x10(void);
-{
-	char alph;
-	
-	int i;
 
-	for (i = 0 ; i < 10; i++)
-	{
-		for(alph = 'a' ; alph <= 'z' : alph++)
-		{
-			putchar(alph);
-		}
-		putchar('\n');
-	}
+/**
+ * print_alphabet_x10 - Print the alphabet 10 times
+ *
+ * Description: This function prints the lowercase alphabet 10 times.
+ */
+void print_alphabet_x10(void)
+{
+    char alph;
+    int i;
+
+    for (i = 0; i < 10; i++)
+    {
+        for (alph = 'a'; alph <= 'z'; alph++)
+        {
+            putchar(alph);
+        }
+        putchar('\n');
+    }
 }
