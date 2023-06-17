c3dec1ad859ea017591b9c2ee944be576d26e876
acostanahuel <acostanahuel17@gmail.com>
2023-06-17 16:42:32 -0700

    fix isalpha

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include "main.h"
 /**
- *_islower - Return 1 if c is a letter or 0 otherwhise
+ *_isalpha - Return 1 if c is a letter or 0 otherwhise
  *@c: input number
  *Return 1 if c is a letter, 0 if else
  */
 int _isalpha(int c)
-{ 
+{
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return  (1);
 	else
