e60d42e5542f1a5c799dd8916adf894c63e95e8c
acostanahuel <acostanahuel17@gmail.com>
2023-06-18 15:10:49 -0700

    times_table

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *times_table - times table
+ *@a: inmput a number
+ *@b: input a number
+ *Return: result
+ *
+ */
+void times_table(void)
+{
+	int a, b;
+
+	for(a = 0; a <= 10; a++)
+	{
+		for(b = 0; b <= 10; b++)
+		{
+			int result= a * b;
+			if (result < 10)
+			{
+				_putchar(result + '0');
+			} else 
+			{
+				_putchar((result / 10) + '0');
+				_putchar((result % 10) + '0');
+			}
+			_putchar('\n');
+		}
+	}
+}
