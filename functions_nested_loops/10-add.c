6172995d1923d202aa266110b85d9a400a56f9bb
acostanahuel <acostanahuel17@gmail.com>
2023-06-18 08:58:47 -0700

    fix add

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *adds two integers and returns the result
+ *@a: a
+ *@b: b
+ *
+ *Return: result
+ */
+int add(int a, int b)
+{
+	
+	return a + b;
+	
+}
