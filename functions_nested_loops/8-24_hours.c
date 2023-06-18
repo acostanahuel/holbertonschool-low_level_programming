54f7b87c2e2e69f3f9c07d5266096f364a97d0a0
acostanahuel <acostanahuel17@gmail.com>
2023-06-18 08:24:50 -0700

    24 fix

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -7,9 +7,9 @@
 void jack_bauer(void)
 {
 	int hour = 0;
-       	int minute = 0;
+	int minute = 0;
 
-	for (hour = 0; hour < 24; hour++) 
+	for (hour = 0; hour < 24; hour++)
 	{
 		for (minute = 0; minute < 60; minute++)
 		{
@@ -19,6 +19,6 @@ void jack_bauer(void)
 			_putchar((minute / 10) + '0');
 			_putchar((minute % 10) + '0');
 			_putchar('\n');
-		}	
+		}
 	}
 }
