8921dfe0c02749fea6cd610334e484f1ab2d8205
acostanahuel <acostanahuel17@gmail.com>
2023-06-17 17:56:10 -0700

    last digit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *print_last_digit - alsdkjaslkdj
+ *@n: input a number
+ *Return: the value of the last digit
+ */
+int print_last_digit(int n)
+{
+	int last = n % 10;
+
+	if (last < 0)
+		last *= -1;
+	_putchar(last + '0');
+	return (last);
+}
