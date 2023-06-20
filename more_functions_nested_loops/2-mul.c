95792a72c0b4e9c497e9b00ccb237d7aa02f1b54
acostanahuel <acostanahuel17@gmail.com>
2023-06-19 18:04:42 -0700

    fix mul

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- *multiples two integers
+ *description: multiples two integers
  *@a: a interger
  *@b: a integer
  *Return: a * b
  *
  */
 int mul(int a, int b)
-{	
+{
 	return (a * b);
 }
