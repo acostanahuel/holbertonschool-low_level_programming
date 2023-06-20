67b1d638665a874f6664aee6c5ef4b1602157434
acostanahuel <acostanahuel17@gmail.com>
2023-06-20 09:47:00 -0700

    print most numbers

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,27 +1,13 @@
 #include "main.h"
-
 /**
- *print_most_numbers
- *
- *@i: inmput number
- *
- *Return: 0 to 9, except 2 and 4
- *
+ * print_most_numbers - no 2 and 4
  */
-
-
 void print_most_numbers(void)
 {
 	int i;
 
-
-	for (i = 0; i < 9; i++)
-	{	
-		if ( i != 2 && i != 4)
-		{
+	for (i = '0'; i <= '9'; i++)
+		if (i != '2' && i != '4')
 			_putchar(i);
-		}
-	
-		_putchar('\n');	
-	}
+	_putchar('\n');
 }
