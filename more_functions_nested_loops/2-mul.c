da7d1ffdd24db91f6c8a5a48233fc3cf8847ab8c
acostanahuel <acostanahuel17@gmail.com>
2023-06-19 18:06:29 -0700

    mul

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- *description: multiples two integers
- *@a: a interger
- *@b: a integer
- *Return: a * b
+ * description: multiples two integers
+ * @a: a interger
+ * @b: b integer
+ * Return: a * b
  *
  */
 int mul(int a, int b)
