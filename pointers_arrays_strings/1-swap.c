c5fea3e43507d5fbf7c6d78bdb34c2f2093a3574
acostanahuel <acostanahuel17@gmail.com>
2023-06-21 10:04:27 -0700

    swap

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * swap_int - aaaaaaaa
+ * @a: number
+ * @b: number
+ *
+ */
+void swap_int(int *a, int *b)
+{
+	int swap
+
+	swap = *x;
+	x* = *y;
+	y* = swap
+}
