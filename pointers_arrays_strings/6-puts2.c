fc5459db0564c177ee9db5a5936149b1dde1b147
acostanahuel <acostanahuel17@gmail.com>
2023-06-22 12:22:32 -0700

    puts

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -8,8 +8,9 @@ puts2(char *str)
 	int i = 0;
 	int len = strlen(str);
 	
-	for (i = 0; i <= len; i++)
-		if (i%2 == 0)
+	for (i = 0; i < len; i++)
+		
+		if ( i%2 == 0)
 		{
 			putchar (str[i]);
 		}
