e5487f223d4dffd246e8b978acb050ae028b40c8
acostanahuel <acostanahuel17@gmail.com>
2023-06-22 14:16:30 -0700

    puts half

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -3,22 +3,23 @@
 #include "main.h"
 
 
-void 
+void
 puts_half(char *str)
 {
 	int i = 0;
 	int length = 0;
 
-	while (str[length++]);
+	while
+		(str[length++]);
 
 	for (i = length / 2; str[i]; putchar(str[i++]));
 
 	putchar('\n');
 }
 
-int lastch()
+int lastch(void)
 {
 	char *str = "text";
 	puts_half(str);
-	return 0;
+	return (0);
 }
