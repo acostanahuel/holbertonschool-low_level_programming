9661d9b86970d0eba3715255deb67bc0f6a39fe1
acostanahuel <acostanahuel17@gmail.com>
2023-06-21 17:51:45 -0700

    print rev

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *print_rev - aaaaa
+ *@s: string
+ * return: bbebebe
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	if ( i == 0);
+	{
+		printf("%s\n", strrev(s));
+	}
+}
