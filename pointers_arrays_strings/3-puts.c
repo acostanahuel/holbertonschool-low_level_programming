c97e0a32171503c9d44cb640e7d27c8d6d940312
acostanahuel <acostanahuel17@gmail.com>
2023-06-21 17:23:51 -0700

    puts

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -2,12 +2,12 @@
 #include "main.h"
 
 /**
- * _puts - aaaaa
- * @str: string 
+ *_puts - aaaaa
+ *@str: string 
  * return: bbebebe
  */
 
-void _puts(char *str) 
+void _puts(char *str)
 {
 	printf("%s\n", str);
 
