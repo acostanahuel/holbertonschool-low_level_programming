453df9f3672da9192b8924fa2277d4dc887b296c
acostanahuel <acostanahuel17@gmail.com>
2023-06-26 16:34:29 -0700

    strcmp

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
+
+	return (*(unsigned char *)s1 - *(unsigned char *)s2);
+}
