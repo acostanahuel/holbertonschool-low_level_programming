c00ccf9f21ae63b3ea84a99af91143ca724d63f4
acostanahuel <acostanahuel17@gmail.com>
2023-06-27 10:46:55 -0700

    leet

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+/**
+ * leet - encode string
+ * @s: encode string
+ * Return: pointer to encoded string
+ */
+char *leet(char *s)
+{
+	char *r = s;
+	char a[] = { 'a', 'e', 'o', 't', 'l' };
+	char n[] = { 4, 3, 0, 7, 1 };
+	int i = 0;
+
+	while (*s)
+	{
+		for (i = 0; i < 5; i++)
+		{
+			if (*s == a[i] || *s == a[i] - 32)
+				*s = n[i] + '0';
+		}
+		s++;
+	}
+	return (r);
+}
