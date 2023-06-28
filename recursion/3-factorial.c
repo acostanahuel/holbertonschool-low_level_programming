4271d81c79d142bfbeb8272a2ac4e7b1fc7b90ed
acostanahuel <acostanahuel17@gmail.com>
2023-06-28 09:35:29 -0700

    factorial

---
+++ recursion/3-factorial.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *factorial - calculate the factorial n
+ *
+ *
+ */
+
+
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+
+	 return (n * factorial (n - 1));
+	}
+}
