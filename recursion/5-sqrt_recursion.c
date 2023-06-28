e17b44eacf3ff0900a7d08fab258467d8c78a9c1
acostanahuel <acostanahuel17@gmail.com>
2023-06-28 16:17:17 -0700

    recursion square

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,11 +1,26 @@
 #include "main.h"
 
+
+int
+findsquare (int n, int i)
+{
+	if( i * i == n)
+		return (i);
+	else if (i == n)
+		return (-1);
+	else
+		return (findsquare(n, ++i));
+	return (0);
+}
+
+
+
+
 int
 _sqrt_recursion(int n)
 {
-	double result;
-
-	result = _sqrt_recursion(n);
-	return (result);
-	return (0);
+	if (n < 0)
+		return (-1);
+	else
+		return (findsquare(n, 1));
 }
