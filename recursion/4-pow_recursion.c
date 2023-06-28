938c56d503927caa09e26dddd7dd1a7b12e7a413
acostanahuel <acostanahuel17@gmail.com>
2023-06-28 13:59:52 -0700

    pow recursion

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -5,9 +5,13 @@
 int 
 _pow_recursion(int x, int y)
 {
-	if (y == 0)
-		return 1;
-	if (x == 0)
-		return 0;
-	return x * _pow_recursion(x, y - 1);
+	int result = x;
+	
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	result *= _pow_recursion(x, y - 1);
+
+	return (result);
 }
