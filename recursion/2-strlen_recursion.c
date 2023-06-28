6b4987fdad2f5055f2b0243e00520b5001e6c381
acostanahuel <acostanahuel17@gmail.com>
2023-06-28 07:50:14 -0700

    recursion len

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,20 +1,21 @@
 #include "main.h"
 
 /**
-*@s: string
+*
 *
 *_strlen_recursion - aaaa
 */
 
 
 int _strlen_recursion(char *s)
-{	
+{
+
 int i = 0;
 
 if (*s)
 {
-i++;
-i += _strlen_recursion (s + 1);
+	i++;
+	i += _strlen_recursion(s + 1);
 }
 
 return (i);
