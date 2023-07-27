105894630e384a7b3f7963701ce1bab339445246
acostanahuel <acostanahuel17@gmail.com>
2023-07-27 13:03:30 -0700

    print list

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -2,22 +2,22 @@
 #include <stdlib.h>
 #include "lists.h"
 
-size_t 
+size_t
 print_list(const list_t *h)
 {
-	
+
 	size_t k = 0;
 
-	
-	while (h)
+
+	while(h)
 	{
-		if (h->str == NULL)
+		if(h->str == NULL)
 			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		k++;
 		h = h->next;
-		
+
 	}
-	return(k);
+	return (k);
 }
