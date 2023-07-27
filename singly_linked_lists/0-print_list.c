28d8aa2ceb58ce8d43b65867e0a30c8797d64f91
acostanahuel <acostanahuel17@gmail.com>
2023-07-27 12:57:18 -0700

    print

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -6,18 +6,17 @@ size_t
 print_list(const list_t *h)
 {
 	
-	const list_t *p;
 	size_t k = 0;
 
-	p = h;
-	while (p)
+	
+	while (h)
 	{
-		if (p->str == NULL)
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("[%d]%s\n", p->len, p->str);
+			printf("[%d]%s\n", h->len, h->str);
 		k++;
-		p=p->next;
+		h = h->next;
 		
 	}
 	return(k);
