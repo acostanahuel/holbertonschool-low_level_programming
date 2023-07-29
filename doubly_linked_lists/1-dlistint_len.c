7a7291e93e7a99ad17c5f27f3075d7da8f65d6b8
acostanahuel <acostanahuel17@gmail.com>
2023-07-29 12:38:19 -0700

    ,

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ *dlistint_len - counts numbr of elements linked
+ *¨@h: the head of the list
+ *Return: the number of elements in list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t elements = 0;
+
+	while (h)
+	{
+		elements++;
+		h = h->next;
+	}
+	return (elements);
+}
